f99c6380e24f92db4e129be517674baecebdc6b9
cdans <ch.dansard@gmail.com>
2021-06-29 12:31:33 +0200

    state

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,12 +2,18 @@ import React, {Component} from "react";
 
 class Footer extends Component {
 
+    state = {
+        name: 'Christoph',
+        age: 23,
+    }
+
     createALert() {
         alert("clicked")
     }
 
-    changed(){
-        console.log("changed")
+    changed = (event) => {
+        console.log("changed", event.target.value)
+        this.setState({name: event.target.value})
     }
 
     render() {
@@ -18,6 +24,7 @@ class Footer extends Component {
                 </h2>
                 <input
                     type="text"
+                    value={this.state.name}
                     onChange={this.changed}
                 />
             </React.Fragment>
